4fc409195ba6810ba3c1d2ac21b7ed21fec68863
Hoa Ngo <hoango1607@gmail.com>
2023-08-12 19:42:40 +0930

    feat: setup env

---
+++ src/constants/config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  description:
+    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, pre-configured with Husky',
+};
